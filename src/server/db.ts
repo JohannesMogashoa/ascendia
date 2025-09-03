126260d45e6ff665b409ff63b73553fa0ed94aaa
Johannes Mogashoa <jomogashoa1993@gmail.com>
2025-09-03 19:50:23 +0200

    feature: update database schema

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-
 import { env } from "~/env";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const createPrismaClient = () =>
 	new PrismaClient({
 		log:
-			env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-	});
+			env.NODE_ENV === "development"
+				? ["query", "error", "warn"]
+				: ["error"],
+	}).$extends(withAccelerate());
 
 const globalForPrisma = globalThis as unknown as {
 	prisma: ReturnType<typeof createPrismaClient> | undefined;
